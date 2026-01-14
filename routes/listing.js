7544fe81e4614f64bc70caf4866816e32c72008c
harshadkazade <kazadeharshad@gmail.com>
2026-01-14 16:19:22 +0530

    removed comments

--- routes/listing.js
+++ routes/listing.js
@@ -6,25 +6,18 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing}= require("../middleware.js");
 const listingController = require("../controller/listing.js");
 
-//index route
 router.get("/", wrapAsync( listingController.index));
 
-//new route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-//show listing route
 router.get("/:id", wrapAsync(listingController.showListings));
 
-//create listing route
 router.post("/", validateListing, isLoggedIn, wrapAsync(listingController.createNewListing));
 
-//edit listing route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
-//update listing route
 router.put("/:id", validateListing, isLoggedIn, isOwner, wrapAsync(listingController.updateListing));
 
-//delete listing route
 router.delete("/:id", isLoggedIn, isOwner, wrapAsync( listingController.destroyListing));
 
 module.exports = router;
